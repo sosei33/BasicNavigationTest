4e1f6343156b18b897c8784abf5d251427f93256
samuelosei <s.osei40@yahoo.com>
2020-03-11 01:37:04 -0400

    update

--- src/test/java/com/cbt/utilities/BrowserFactory.java
+++ src/test/java/com/cbt/utilities/BrowserFactory.java
@@ -12,26 +12,27 @@ public class BrowserFactory {
 
     //public WebDriver driver;
 
-    public static WebDriver getDriver (String browser){
+    public static WebDriver getDriver(String browser) {
 
-        if(browser.equalsIgnoreCase("chrome")) {
+        if (browser.equalsIgnoreCase("chrome")) {
             WebDriverManager.chromedriver().setup();
             return new ChromeDriver();
-        }else if (browser.equalsIgnoreCase("firefox")){
+        } else if (browser.equalsIgnoreCase("firefox")) {
             WebDriverManager.firefoxdriver().version("79.0").setup();
             return new FirefoxDriver();
-        }else if (browser.equalsIgnoreCase("safari")){
-            //WebDriverManager.safaridriver.setup();
+        } else if (browser.equalsIgnoreCase("safari")) {
             return new SafariDriver();
-            //}else if (browser.equalsIgnoreCase("opera")){
-            //WebDriverManager.operadriver().setup();
-            //return new OperaDriver();
-           //}else if (browser.equalsIgnoreCase("edge")){
-            //WebDriverManager.edgedriver().version("80.0.361.66").setup();
-            //return null; //new EdgeDriver();
-        }else { //type in "edge" in the driver and you get null
+        } else {
             return null;
         }
     }
+}//}else if (browser.equalsIgnoreCase("opera")){
+//WebDriverManager.operadriver().setup();
+//return new OperaDriver();
+//}else if (browser.equalsIgnoreCase("edge")){
+//WebDriverManager.edgedriver().version("80.0.361.66").setup();
+//return null; //new EdgeDriver();
 
-}
+//WebDriverManager.safaridriver.setup();
+
+//type in "edge" in the driver and you get null
